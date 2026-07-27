c905bcb27852be4a94a72320a37c910915a6602c
igorhghd <igorhenrique.gomes@hotmail.com>
2026-07-27 15:34:26 -0300

    initial commit

---
+++ src/main/java/henrique/igor/iiot/IndustryIiotApplication.java
@@ -0,0 +1,13 @@
+package henrique.igor.iiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustryIiotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndustryIiotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/henrique/igor/iiot/IndustryIiotApplicationTests.java
@@ -0,0 +1,13 @@
+package henrique.igor.iiot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustryIiotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
